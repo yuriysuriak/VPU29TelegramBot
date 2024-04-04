9562b37a2e709ae0a3350c1c79010af0f6920b66
Oleksiy <oleksaww99@gmail.com>
2024-04-04 15:45:24 +0300

    updated requirements.txt

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
 import os
 import logging
-
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
-
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 load_dotenv()
-
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-
-
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    keyboard = [
+        [KeyboardButton("Hello")],
+        [KeyboardButton('Share my location', request_location=True)],
+        [KeyboardButton('Share my contact', request_contact=True)]
+        ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Im a bot, please talk to me!',
+        reply_markup=reply_markup
+    )
 
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    lat = update.message.location.latitude
+    lon = update.message.location.longitude
+    await update.message.reply_text(f'lat = {lat}, lon = {lon}')
+
+async def contact(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.contact.user_id
+    first_name = update.message.contact.first_name
+    last_name = update.message.contact.last_name
+    await update.message.reply_text(
+        f"""
+        Id = {user_id}
+        First Name = {first_name}
+        Last Name = {last_name}
+        """,
+        reply_markup = ReplyKeyboardRemove()
+    )
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+    hello_handler = CommandHandler('hello', hello)
+    application.add_handler(hello_handler)
+    location_handler = MessageHandler(filters.LOCATION, location)
+    application.add_handler(location_handler)
+    contact_handler = MessageHandler(filters.CONTACT, contact)
+    application.add_handler(contact_handler)
+
 
     application.run_polling()
